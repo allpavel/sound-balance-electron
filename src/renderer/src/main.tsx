3bcdc0fea1c4c1e5f238929e0279e1412a67c671
Pavel Alloyarov <platprincipal.adv@gmail.com>
2025-08-19 19:12:21 +0300

    add rtk store

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@mantine/core/styles.css";
+import { Provider as RTKProvider } from "react-redux";
+import { store } from "./store/store";
 
 const theme = createTheme({
 	cursorType: "pointer",
@@ -12,8 +14,10 @@ const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
 	<StrictMode>
-		<MantineProvider theme={theme}>
-			<App />
-		</MantineProvider>
+		<RTKProvider store={store}>
+			<MantineProvider theme={theme}>
+				<App />
+			</MantineProvider>
+		</RTKProvider>
 	</StrictMode>,
 );
---
+++ src/renderer/src/store/tracksSlice.ts
@@ -0,0 +1,22 @@
+import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import type { Metadata } from "types";
+import type { RootState } from "./store";
+
+export const tracksAdapter = createEntityAdapter<Metadata>({
+	sortComparer: (a: Metadata, b: Metadata) => a.id.localeCompare(b.id),
+});
+
+export const tracksSlice = createSlice({
+	name: "tracks",
+	initialState: tracksAdapter.getInitialState(),
+	reducers: {
+		loadTracks: tracksAdapter.setAll,
+	},
+});
+
+const selectors = tracksAdapter.getSelectors();
+export const { selectAll: selectAllTracks, selectById: selectTrackById } =
+	selectors;
+
+export const { loadTracks } = tracksSlice.actions;
+export default tracksSlice.reducer;
