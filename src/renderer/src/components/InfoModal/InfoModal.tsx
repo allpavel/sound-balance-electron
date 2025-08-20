a652d51377f7db6233117eb9e538012df0695bd8
Pavel Alloyarov <platprincipal.adv@gmail.com>
2025-08-20 20:02:46 +0300

    add a modal

--- src/renderer/src/components/InfoModal/InfoModal.tsx
+++ src/renderer/src/components/InfoModal/InfoModal.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "@mantine/core";
+import { Image, Modal, Table } from "@mantine/core";
 import type { RootState } from "@renderer/store/store";
 import { selectTrackById } from "@renderer/store/tracksSlice";
 import { useSelector } from "react-redux";
@@ -17,19 +17,123 @@ export default function InfoModal({
 	const trackData = useSelector((state: RootState) =>
 		selectTrackById(state.tracks, trackId),
 	);
-
 	if (!trackData) return null;
 
 	return (
-		<Modal
-			opened={isOpen}
-			onClose={onClose}
-			title="Details"
-			size={"lg"}
-			padding={"xl"}
-			overlayProps={{ blur: 3 }}
-		>
-			{trackData.common.artist}
-		</Modal>
+		<Modal.Root opened={isOpen} onClose={onClose}>
+			<Modal.Overlay blur={5} />
+			<Modal.Content>
+				<Modal.Header bg={"cyan"}>
+					<Modal.Title fw={"bold"}>{trackData.file}</Modal.Title>
+					<Modal.CloseButton aria-label="Close modal" />
+				</Modal.Header>
+				<Modal.Body>
+					<Table
+						stickyHeader
+						stickyHeaderOffset={60}
+						verticalSpacing={"sm"}
+						highlightOnHover
+						withColumnBorders
+					>
+						<Table.Thead>
+							<Table.Tr>
+								<Table.Th>Tag</Table.Th>
+								<Table.Th>Value</Table.Th>
+							</Table.Tr>
+						</Table.Thead>
+						<Table.Tbody>
+							{Object.entries(trackData.common).map(([key, value]) => {
+								if (Array.isArray(value)) {
+									if (key === "picture") {
+										return (
+											<Table.Tr key={key}>
+												<Table.Td>Album Art</Table.Td>
+												<Table.Td>
+													{value.map((item, index) => (
+														<Image
+															fit="contain"
+															src={`data:${item.format};base64,${item.data}`}
+															key={`${key}-${index + 1}`}
+														/>
+													))}
+												</Table.Td>
+											</Table.Tr>
+										);
+									}
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>{value.join(", ")}</Table.Td>
+										</Table.Tr>
+									);
+								} else if (typeof value === "object" && value !== null) {
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>
+												{value.no && value.of
+													? `${value.no} of ${value.of}`
+													: "n/a"}
+											</Table.Td>
+										</Table.Tr>
+									);
+								} else {
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>{value ?? "n/a"}</Table.Td>
+										</Table.Tr>
+									);
+								}
+							})}
+							{Object.entries(trackData.format).map(([key, value]) => {
+								if (Array.isArray(value)) {
+									if (key === "picture") {
+										return (
+											<Table.Tr key={key}>
+												<Table.Td>Album Art</Table.Td>
+												<Table.Td>
+													{value.map((item, index) => (
+														<Image
+															fit="contain"
+															src={`data:${item.format};base64,${item.data}`}
+															key={`${key}-${index + 1}`}
+														/>
+													))}
+												</Table.Td>
+											</Table.Tr>
+										);
+									}
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>{value.join(", ")}</Table.Td>
+										</Table.Tr>
+									);
+								} else if (typeof value === "object" && value !== null) {
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>
+												{value.no && value.of
+													? `${value.no} of ${value.of}`
+													: "n/a"}
+											</Table.Td>
+										</Table.Tr>
+									);
+								} else {
+									return (
+										<Table.Tr key={key}>
+											<Table.Td>{key}</Table.Td>
+											<Table.Td>{value ?? "n/a"}</Table.Td>
+										</Table.Tr>
+									);
+								}
+							})}
+						</Table.Tbody>
+					</Table>
+				</Modal.Body>
+			</Modal.Content>
+		</Modal.Root>
 	);
 }
--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 import type { IAudioMetadata } from "music-metadata";
 
-export type Metadata = IAudioMetadata & { id: string };
+export type Metadata = IAudioMetadata & { id: string; file: string };
 
 export type NativeValue = {
 	data: Uint8Array | string;
