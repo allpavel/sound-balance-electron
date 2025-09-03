729a502f3781e89b1d4fe24045695de99bafa483
Pavel Alloyarov <platprincipal.adv@gmail.com>
2025-09-03 19:17:03 +0300

    refactor processAlbumCover

--- src/main/lib/processAlbumCover.ts
+++ src/main/lib/processAlbumCover.ts
@@ -2,7 +2,7 @@ import type { IAudioMetadata } from "music-metadata";
 import type { NativeValue } from "../../../types";
 
 export const processAlbumCover = (metadata: IAudioMetadata) => {
-	const id3v23 = metadata.native?.["ID3v2.3"];
+	const id3v23 = metadata.native?.["ID3v2.3"] || metadata.native?.["ID3v2.4"];
 	if (!id3v23) return metadata;
 
 	const apicFrame = id3v23.find((item) => item.id === "APIC");
