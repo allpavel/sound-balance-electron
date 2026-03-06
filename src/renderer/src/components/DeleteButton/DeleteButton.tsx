5599759d976b0a70d48fb9382afcb9021af6d71e
Pavel Alloyarov <platprincipal.adv@gmail.com>
2026-03-06 21:51:24 +0300

    fix(delete-button): use correct selector for selected tracks
    The Delete Button was referencing the old selected tracks selector. Update to use
    the new `selectAllSelectedTracks` selector from the tracks slice, restoring
    the delete functionality.

--- src/renderer/src/components/DeleteButton/DeleteButton.tsx
+++ src/renderer/src/components/DeleteButton/DeleteButton.tsx
@@ -1,15 +1,17 @@
 import { Button, useMantineTheme } from "@mantine/core";
 import { useAppDispatch } from "@renderer/hooks/useAppDispatch";
 import { useAppSelector } from "@renderer/hooks/useAppSelector";
-import { getAllSelectedTracks } from "@renderer/store/slices/selectedTracksSlice";
-import { removeTracks } from "@renderer/store/slices/tracksSlice";
+import {
+	removeTracks,
+	selectAllSelectedTracks,
+} from "@renderer/store/slices/tracksSlice";
 import { IconTrash } from "@tabler/icons-react";
 
 export default function DeleteButton() {
 	const dispatch = useAppDispatch();
 	const theme = useMantineTheme();
 	const selectedTracks = useAppSelector((state) =>
-		getAllSelectedTracks(state.tracks),
+		selectAllSelectedTracks(state.tracks),
 	).map((track) => track.id);
 
 	const deleteSelectedTracks = () => {
