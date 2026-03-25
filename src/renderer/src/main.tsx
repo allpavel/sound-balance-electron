eb526d6881b94b98bfddff81800e84c2a2362f4e
Pavel Alloyarov <platprincipal.adv@gmail.com>
2026-03-25 22:10:56 +0300

    feat(app): add QueryClientProvider
    - Wrap app with QueryClientProvider in main.tsx

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as RTKProvider } from "react-redux";
 import { store } from "./store/store";
 
@@ -10,14 +11,18 @@ const theme = createTheme({
 	cursorType: "pointer",
 });
 
+const queryClient = new QueryClient();
+
 const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
 	<StrictMode>
-		<RTKProvider store={store}>
-			<MantineProvider theme={theme}>
-				<App />
-			</MantineProvider>
-		</RTKProvider>
+		<QueryClientProvider client={queryClient}>
+			<RTKProvider store={store}>
+				<MantineProvider theme={theme}>
+					<App />
+				</MantineProvider>
+			</RTKProvider>
+		</QueryClientProvider>
 	</StrictMode>,
 );
