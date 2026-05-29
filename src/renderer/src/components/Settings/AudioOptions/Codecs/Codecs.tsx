cfdf292c91f392eb460b10f5b4e0c007346b5ff6
Pavel Alloyarov <platprincipal.adv@gmail.com>
2026-05-29 20:30:16 +0300

    feat(settings): add Codecs component with grouped audio encoder options
    - Create Codecs.tsx component using Mantine Select with grouped data
    - Transform audioEncoderGroups into grouped options format
    - Enable dynamic rendering of encoder categories

---
+++ src/renderer/src/components/Settings/AudioOptions/Codecs/Codecs.tsx
@@ -0,0 +1,43 @@
+import { Select } from "@mantine/core";
+import type { UseFormReturnType } from "@mantine/form";
+import { audioEncoderGroups } from "@renderer/components/Settings/audioCodecs";
+import type { SettingsForm } from "@renderer/components/Settings/settings.types";
+
+const data = audioEncoderGroups.map((item) => ({
+	group: item.category,
+	items: item.encoders.map((encoder) => ({
+		value: encoder.value,
+		label: encoder.label,
+	})),
+}));
+
+/*
+ * sound-balance-electron
+ * Copyright (C) 2026 Pavel Alloyarov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+export default function Codecs({
+	form,
+}: {
+	form: UseFormReturnType<SettingsForm>;
+}) {
+	return (
+		<Select
+			label="Audio codec:"
+			data={data}
+			{...form.getInputProps("audio.audioCodec")}
+		/>
+	);
+}
